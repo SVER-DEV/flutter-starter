a48ab5353177f3fa347e029965ae9858ba0124da
sh4lluloveme <wh3n_gg@kakao.com>
2024-01-02 19:52:21 +0900

    ✨ :: 첫 커밋

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '운지소년'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: SvgPicture.asset('images/logo.svg', width: 30, height: 30),
+        actions: <Widget>[Padding(padding: EdgeInsets.only(right:20), child: SvgPicture.asset('images/search.svg', width: 23, height: 23)), ],
+    ),
+
+
+      body: Center(
+
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
