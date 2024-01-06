fef8fbcedf9292cc847c473d6f2be738409ae1ef
Jihyuk Lee <wh3n_gg@kakao.com>
2024-01-06 23:57:52 +0900

    🌐 :: 프로젝트 주제 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,13 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '하루일기',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.black, background: Colors.grey.shade900),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '테스트'),
+      home: const MyHomePage(title: 'mainpage'),
     );
   }
 }
@@ -32,35 +31,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: SvgPicture.asset('images/logo.svg', width: 30, height: 30),
-        actions: <Widget>[Padding(padding: EdgeInsets.only(right:20), child: SvgPicture.asset('images/search.svg', width: 23, height: 23)), ],
-    ),
+        appBar: AppBar(
+            backgroundColor: Colors.grey.shade900,
+            titleTextStyle: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+            centerTitle: true,
+            title: Text("12월 25일"),
+            leadingWidth: 44,
+            leading: Padding(
+                padding: EdgeInsets.only(left: 10),
+                child:
+                    SvgPicture.asset('images/back.svg', width: 1, height: 1))),
+        body: Center(
+            child: Align(
+          alignment: Alignment.topLeft,
+          child: Stack(children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  child: Text(
+                    "벼루님, \n오늘 기분 어때요?",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )),
+            Align(
+                alignment: Alignment.center,
+                child: SvgPicture.asset(
+                  'images/hamster.svg',
+                  width: 100,
+                  height: 200,
+                )),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Card(
+                  color: Colors.grey.shade800,
+                  child: Container(
+                    height: 200.0,
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                            child: Text("기분",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.5)),
+                          ),
+                        ),
+                        Row(
+                          children: [],
 
-
-      body: Center(
-
-        child: Column(
-          children: <Widget>[
-
-          ],
-        ),
-      ),
-       // This trailing comma makes auto-formatting nicer for build methods.
-    );
+                        )
+                      ],
+                    ),
+                  )),
+            )
+          ]),
+        )));
   }
 }
